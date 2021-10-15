11f81152cd5c135439606e7774a29bba77059038
AbbosB12 <abbosboev@gmail.com>
2021-10-15 19:09:57 -0400

    LoginPage class is created

--- src/test/java/com/trycloud/pages/LoginPage.java
+++ src/test/java/com/trycloud/pages/LoginPage.java
@@ -1,4 +1,27 @@
 package com.trycloud.pages;
 
+import com.trycloud.utilities.ConfigReader;
+import com.trycloud.utilities.Driver;
+import jdk.nashorn.internal.runtime.regexp.joni.Config;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+
 public class LoginPage {
+
+    @FindBy(id = "user")
+    private WebElement username;
+
+    @FindBy(id = "password")
+    private WebElement password;
+
+    @FindBy(id = "submit-form")
+    private WebElement login;
+    public LoginPage(){
+        PageFactory.initElements(Driver.getDriver(), this);
+    }
+
+    public void goTo(){
+Driver.getDriver().get(ConfigReader.read("trycloud_url"));
+    }
 }
