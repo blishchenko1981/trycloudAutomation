4101b463bfc99ea0f4a016b18331c4a23a09f8ae
AbbosB12 <abbosboev@gmail.com>
2021-10-15 19:17:23 -0400

    LoginPage with login method

--- src/test/java/com/trycloud/pages/LoginPage.java
+++ src/test/java/com/trycloud/pages/LoginPage.java
@@ -2,7 +2,6 @@ package com.trycloud.pages;
 
 import com.trycloud.utilities.ConfigReader;
 import com.trycloud.utilities.Driver;
-import jdk.nashorn.internal.runtime.regexp.joni.Config;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
@@ -17,11 +16,18 @@ public class LoginPage {
 
     @FindBy(id = "submit-form")
     private WebElement login;
+
     public LoginPage(){
         PageFactory.initElements(Driver.getDriver(), this);
     }
 
     public void goTo(){
 Driver.getDriver().get(ConfigReader.read("trycloud_url"));
+    }
+
+    public void login(String username,String password){
+        this.username.sendKeys(ConfigReader.read(username));
+        this.password.sendKeys(ConfigReader.read(password));
+        this.login.click();
     }
 }
